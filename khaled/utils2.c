3ae8289175f86ab986fb237bf664f3711eb576c3
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-10-16 03:45:51 -0700

    test

--- khaled/utils2.c
+++ khaled/utils2.c
@@ -9,15 +9,31 @@
 
 int _strcmp(const char *str1, const char *str2)
 {
-	int i, res = 0;
+	int i;
 
-	for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++) {
-		if (str1[i] != str2[i]) {
-			res = -1;
+	if (str1 == NULL && str2 == NULL)
+	{
+		return (0);
+	}
+    
+	if (str1 == NULL || str2 == NULL) {
+		return (-1);
+	}
+
+	for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++)
+	{
+		if (str1[i] != str2[i])
+		{
+			return (-1);
 		}
 	}
 
-	return (res);
+	if (str1[i] == '\0' && str2[i] == '\0')
+	{
+		return (0);
+	}
+
+	return (-1);
 }
 
 
